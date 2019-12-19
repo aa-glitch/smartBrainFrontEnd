629f2aa1862cd42879e336d008cbc20504a41149
Anshul Anand <ansh.anand@gmail.com>
2019-12-18 22:17:22 -0500

    second commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+    <div>
+        <div className = 'white f3'>
+            {'Anshul, your current rank is...'}
+        </div>
+        <div className='white f1'>
+            {'5'}
+        </div>
+    </div>
+    );
+}
+
+export default Navigation;
